12ac54196c38b69535f19985827192976c791b10
Martijn <mrtijn@riseup.net>
2018-12-13 16:54:31 +0100

    Add basic HTML version

---
+++ src/app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+    app.py
+
+    Copyright (c) 2018 Martijn
+
+    This file is part of ippls.
+
+    ippls is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    ippls is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with ippls.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+from flask import Flask
+from flask import json
+from flask import request
+from flask import Response
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def html():
+    user_ip = request.remote_addr
+    return render_template("index.html", user_ip = user_ip)
