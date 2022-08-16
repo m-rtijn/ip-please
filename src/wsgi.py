3938a5a37fe793b0ab8ee7cada48fe69efa33cd9
Martijn <martijn@mrtijn.nl>
2022-08-16 09:26:38 +0200

    Rename ippls to ip-please

--- src/wsgi.py
+++ src/wsgi.py
@@ -4,23 +4,23 @@
 
     Copyright (c) 2019 Martijn
 
-    This file is part of ippls.
+    This file is part of ip-please.
 
-    ippls is free software: you can redistribute it and/or modify
+    ip-please is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    ippls is distributed in the hope that it will be useful,
+    ip-please is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with ippls.  If not, see <https://www.gnu.org/licenses/>.
+    along with ip-please.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-from ippls import app
+from ip_please import app
 
 if __name__ == "__main__":
     app.run()
